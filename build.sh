67b34a24405350f8dd48a7adf65bcf24004f836b
daniel <github@dbauer.me>
2023-09-23 21:26:49 +0200

    set all symbols on symbols layer

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -e
-
-west build -p -b nice_nano_v2 -- -DSHIELD="corax56_left nice_view_adapter nice_view" -DZMK_CONFIG=/workspaces/zmk-config/config
+FLAGS="-p"
+FLAGS=""
+west build $FLAGS -b nice_nano_v2 -- -DSHIELD="corax56_left nice_view_adapter nice_view" -DZMK_CONFIG=/workspaces/zmk-config/config
 mv /workspaces/zmk/app/build/zephyr/zmk.uf2  /workspaces/zmk-config/left.uf2
-west build -p -b nice_nano_v2 -- -DSHIELD="corax56_right nice_view_adapter nice_view" -DZMK_CONFIG=/workspaces/zmk-config/config
+west build $FLAGS -b nice_nano_v2 -- -DSHIELD="corax56_right nice_view_adapter nice_view" -DZMK_CONFIG=/workspaces/zmk-config/config
 mv /workspaces/zmk/app/build/zephyr/zmk.uf2 /workspaces/zmk-config/right.uf2
