3f826d6f8c95175608ad3183e4604c276efa9bd4
gaston <gaston@gassagosso.com>
2024-04-17 14:09:39 +0200

    remove niceview

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 set -e
 FLAGS="-p"
 
-west build $FLAGS -b nice_nano_v2 -- -DSHIELD="corax_left nice_view" -DZMK_CONFIG=/workspaces/zmk-config/config
+west build $FLAGS -b nice_nano_v2 -- -DSHIELD="corax_left" -DZMK_CONFIG=/workspaces/zmk-config/config
 mv /workspaces/zmk/app/build/zephyr/zmk.uf2  /workspaces/zmk-config/left.uf2
-west build $FLAGS -b nice_nano_v2 -- -DSHIELD="corax_right nice_view" -DZMK_CONFIG=/workspaces/zmk-config/config
+west build $FLAGS -b nice_nano_v2 -- -DSHIELD="corax_right" -DZMK_CONFIG=/workspaces/zmk-config/config
 mv /workspaces/zmk/app/build/zephyr/zmk.uf2 /workspaces/zmk-config/right.uf2
